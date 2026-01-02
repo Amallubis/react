4699d68b7983f08b72a4c5bb5c67d4d215defee5
Amallubis <amal.lubis@gmail.com>
2026-01-02 22:40:34 +0700

    belajar reactjs

---
+++ src/component/usercard.jsx
@@ -0,0 +1,10 @@
+function UserCard ({name, age}) {
+  return (
+  <div>
+    <h1>{name}</h1>
+      <p>umur: {age}</p>
+  </div>
+  );
+}
+
+export default UserCard;
---
+++ src/component/welcome.jsx
@@ -0,0 +1,12 @@
+
+function Welcome(props) {
+  return (
+    <div>
+    <h1>Halo, {props.name}</h1>
+    <p>Umur, {props.age}</p>
+    </div>
+  )
+
+}
+
+export default Welcome
